46498f0452944434ea908551ecc9415bf40a7b73
patchaiy <50619625+patchaiy@users.noreply.github.com>
2019-06-13 17:07:45 +0530

    Update and rename ranger.py to rangers.py

--- rangers.py
+++ rangers.py
@@ -1,6 +1,6 @@
-n1 = int(input())
-n2 = int(input())
-for num in range(n1, n2+ 1):
+low = int(input())
+upp= int(input())
+for num in range(low, upp+ 1):
    if num > 1:
        for i in range(2,num):
            if (num % i) == 0:
