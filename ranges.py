fee8492ccd721984a827cae633577f28a1a94970
patchaiy <50619625+patchaiy@users.noreply.github.com>
2019-06-13 16:51:53 +0530

    Update and rename range.py to ranges.py

--- ranges.py
+++ ranges.py
@@ -1,10 +1,6 @@
 lower = int(input())
 upper = int(input())
-
-print("Prime numbers between",lower,"and",upper,"are:")
-
 for num in range(lower,upper + 1):
-   
    if num > 1:
        for i in range(2,num):
            if (num % i) == 0:
